00e04d88ff87b9cb9a537276c372c7c5677ae0d2
gabrielstremsdoerfer <gabriel.stremsdoerfer@eleve.ensai.fr>
2025-09-05 09:38:51 +0000

    modif classe abstraite et sous-classes

---
+++ src/business_object/pokemon/all_rounder_pokemon.py
@@ -0,0 +1,10 @@
+from business_object.pokemon.abstract_pokemon import AbstractPokemon
+
+
+class AllRounderPokemon(AbstractPokemon):
+    def __init__(self):
+        super().__init__()
+
+    def get_pokemon_attack_coef(self):
+        multiplier = 1 + (self.sp_atk_current + self.sp_def_current) / 200
+        return multiplier
